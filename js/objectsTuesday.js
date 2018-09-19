05317993278838387e6d40471c87ae2fc5846833
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2018-09-18 22:26:24 -0700

    finished main js task.  Slight work in CSS

--- js/objectsTuesday.js
+++ js/objectsTuesday.js
@@ -3,7 +3,6 @@
 
 var globalTime = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm','6:00pm', '7:00pm', '8:00pm'];
 
-// Constructor
 function PatCookiesLocation (locationName, minCust, maxCust, avgCookiesPerCust, idPat) {
   this.locationName = locationName;
   this.minCust = minCust;
@@ -13,9 +12,9 @@ function PatCookiesLocation (locationName, minCust, maxCust, avgCookiesPerCust, 
   this.hourlyCustomersArray = [];
   this.hourlySalesArray = [];
   this.totalSales = 0;
-};
+  //this.totalByHour = 0;
+}
 
-// Prototype to generate hourly customers
 PatCookiesLocation.prototype.calcHourlyCustomers = function() {
   for(var i = 0; i < globalTime.length; i++) {
     var randomCust = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
@@ -23,36 +22,34 @@ PatCookiesLocation.prototype.calcHourlyCustomers = function() {
   }
 };
 
-// Prototype to generate hourly sales
 PatCookiesLocation.prototype.calcHourlySales = function() {
   for(var i = 0; i < globalTime.length; i++) {
     var cookiesSoldThatHour = this.hourlyCustomersArray[i] * this.avgCookiesPerCust;
     this.hourlySalesArray.push(cookiesSoldThatHour);
     this.totalSales += cookiesSoldThatHour;
+    //this.totalByHour = some function to add cookies by hours
   }
 };
 
-// Prototype to trigger render of unordered list
 function makeHeaderRow() {
   var cookieTable = document.getElementById('fullTable');
   var thEl = document.createElement('th');
   var trEl = document.createElement('tr');
   thEl.textContent = 'Locations';
   trEl.appendChild(thEl);
-  // cookieTable.appendChild(trEl);
 
-  var thEl = document.createElement('th');
+  for (var i = 0; i < globalTime.length; i++) {
+    thEl = document.createElement('th');
+    thEl.textContent = globalTime[i];
+    trEl.appendChild(thEl);
+  }
+
+  thEl = document.createElement('th');
   thEl.textContent = 'Daily Total';
   trEl.appendChild(thEl);
 
-  for (var i = 0; i < globalTime.length; i++) {
-    var thEl = document.createElement('th');
-    thEl.textContent = globalTime[i];
-    trEl.appendChild(thEl);
-  };
-
   cookieTable.appendChild(trEl);
-};
+}
 makeHeaderRow();
 
 PatCookiesLocation.prototype.render = function() {
@@ -65,20 +62,19 @@ PatCookiesLocation.prototype.render = function() {
   trEl.appendChild(tdEl);
   // cookieTable.appendChild(trEl);
 
-  var tdEl = document.createElement('td');
-  tdEl.textContent = Math.round(this.totalSales);
-  trEl.appendChild(tdEl);
-
   for (var i = 0; i < globalTime.length; i++) {
-    var tdEl = document.createElement('td');
+    tdEl = document.createElement('td');
     tdEl.textContent = Math.round(this.hourlySalesArray[i]);
     trEl.appendChild(tdEl);
-  };
+  }
+
+  tdEl = document.createElement('td');
+  tdEl.textContent = Math.round(this.totalSales);
+  trEl.appendChild(tdEl);
 
   cookieTable.appendChild(trEl);
 };
 
-// All instances and calls of prototypes to calculate hourly customers, hourly sales, and then render on the page
 //function PatCookiesLocation (locationName, minCust, maxCust, avgCookiesPerCust, idPat)
 var firstAndPike = new PatCookiesLocation('First and Pike', 23, 65, 6.3, 'pikeId');
 firstAndPike.render();
@@ -95,5 +91,16 @@ capHill.render();
 var alki = new PatCookiesLocation('Alki', 2, 16, 4.6, 'alkiId');
 alki.render();
 
-var totalSales5 = new PatCookiesLocation('Total by hour', 'create for loop', 1, 2, 'allId');
-totalSales5.render();
+var totalPerHour = new PatCookiesLocation('Total by hour', '', '', '', 'totalPerHId');
+totalPerHour.render();
+
+//Making total by hour
+//var locationArray = [firstAndPike, seatacAirport, seattleCenter, capitolHill, alki];
+// function totalByHour (time, locationArray) {
+//   this.time = time;
+//   this.locationArray = locationArray;
+//   this.hourlyCustomersArray = [];
+//   this.hourlySalesArray = [];
+//   this.totalSales = 0;
+//   //this.totalByHour = 0;
+// }
